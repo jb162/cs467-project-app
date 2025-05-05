19d1d9bbe2be7d5acdde90ea042fc7262ae78c70
Sam Carlson <samcarlson@sams-mbp.mynetworksettings.com>
2025-05-05 15:31:00 -0400

    Made small edit based on feedback / added 'Reset Password' button

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,66 +1,55 @@
-import { StyleSheet, SafeAreaView, View, TouchableOpacity, TextInput, FlatList, Image, RefreshControl, Text } from 'react-native';
+import { StyleSheet, SafeAreaView, View, TouchableOpacity, TextInput, FlatList, Image, RefreshControl } from 'react-native';
 import Ionicons from '@expo/vector-icons/Ionicons';
 import { useState } from 'react';
 import ProductCard from '../ProductCard';
-import FilterComponent from '../filter';
 
+const products = [
+  {
+    id: 1,
+    image: 'https://images.unsplash.com/photo-1714070700737-24acfe6b957c?q=80&w=2960&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    title: 'White T-shirt',
+    price: 20,
+  },
+  {
+    id: 2,
+    image: 'https://images.unsplash.com/photo-1704919266475-aa6302e25209?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    title: 'Nike Air Jordans',
+    price: 120,
+  },
+  {
+    id: 3,
+    image: 'https://plus.unsplash.com/premium_photo-1681160405609-389cd83998d0?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8bWFjJTIwYm9va3xlbnwwfHwwfHx8MA%3D%3D',
+    title: '2018 Macbook Pro',
+    price: 1000,
+  },
+  {
+    id: 4,
+    image: 'https://images.unsplash.com/photo-1714070700737-24acfe6b957c?q=80&w=2960&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    title: 'White T-shirt',
+    price: 20,
+  },
+  {
+    id: 5,
+    image: 'https://images.unsplash.com/photo-1704919266475-aa6302e25209?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    title: 'Nike Air Jordans',
+    price: 120,
+  },
+  {
+    id: 6,
+    image: 'https://plus.unsplash.com/premium_photo-1681160405609-389cd83998d0?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8bWFjJTIwYm9va3xlbnwwfHwwfHx8MA%3D%3D',
+    title: '2018 Macbook Pro',
+    price: 1000,
+  },
+];
 
 export default function Index() {
-  const initialProducts = [
-    {
-      id: 1,
-      image: 'https://images.unsplash.com/photo-1714070700737-24acfe6b957c?q=80&w=2960&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      title: 'White T-shirt',
-      price: 20,
-    },
-    {
-      id: 2,
-      image: 'https://images.unsplash.com/photo-1704919266475-aa6302e25209?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      title: 'Nike Air Jordans',
-      price: 120,
-    },
-    {
-      id: 3,
-      image: 'https://plus.unsplash.com/premium_photo-1681160405609-389cd83998d0?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8bWFjJTIwYm9va3xlbnwwfHwwfHx8MA%3D%3D',
-      title: '2014 Macbook Pro',
-      price: 800,
-    },
-    {
-      id: 4,
-      image: 'https://images.unsplash.com/photo-1714070700737-24acfe6b957c?q=80&w=2960&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      title: 'Classic White T-shirt',
-      price: 40,
-    },
-    {
-      id: 5,
-      image: 'https://images.unsplash.com/photo-1704919266475-aa6302e25209?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      title: 'Old Nike Air Jordans',
-      price: 500,
-    },
-    {
-      id: 6,
-      image: 'https://plus.unsplash.com/premium_photo-1681160405609-389cd83998d0?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8bWFjJTIwYm9va3xlbnwwfHwwfHx8MA%3D%3D',
-      title: '2018 Macbook Pro',
-      price: 1000,
-    },
-  ];
-
-  const [allProducts, setAllProducts] = useState(initialProducts);
-  const [products, setProducts] = useState(initialProducts);
   const [inputTerm, setInputTerm] = useState('');
   const [refreshing, setRefreshing] = useState(false);
-  const [showFilter, setShowFilter] = useState(false);
 
   const handleSearch = () => {
-    if (inputTerm.trim() === '') {
-      setProducts(allProducts);
-    } else {
-      const filtered = products.filter(product =>
-        product.title.toLowerCase().includes(inputTerm.toLowerCase())
-      );
-      setProducts(filtered);
-    }
-  };
+    // Testing search functionality
+    console.log('Search term:', inputTerm);
+  }
 
   const handleRefresh = () => {
     // Testing refresh functionality
@@ -71,20 +60,6 @@ export default function Index() {
     }, 1000);
   };
 
-  const handleSort = (sortOption) => {
-    let sortedProducts = [...products];
-
-    if (sortOption === 'lowToHigh') {
-      sortedProducts.sort((a, b) => a.price - b.price);
-    } else if (sortOption === 'highToLow') {
-      sortedProducts.sort((a, b) => b.price - a.price);
-    } else if (sortOption === 'alphabetical') {
-      sortedProducts.sort((a, b) => a.title.localeCompare(b.title));
-    }
-
-    setProducts(sortedProducts);
-  };
-
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: '#f2f2f2' }}>
       <View style={styles.header}>
@@ -93,7 +68,7 @@ export default function Index() {
             <TouchableOpacity
               accessibilityLabel="search button"
               accessibilityRole="button"
-              onPress={handleSearch}
+              onPress={() => {/* placeholder functionality */}}
             >
               <Ionicons name="search" size={24} color="black" style={{ marginRight: 8 }}/>
             </TouchableOpacity>
@@ -112,19 +87,13 @@ export default function Index() {
             <TouchableOpacity
               accessibilityLabel="filter button"
               accessibilityRole="button"
-              onPress={() => setShowFilter(true)}
+              onPress={() => {/* placeholder functionality */}}
             >
               <Ionicons name="filter" size={24} color="black" style={{ marginLeft: 8 }}/>
             </TouchableOpacity>
           </View>
         </View>
       </View>
-
-      {products.length === 0 && (
-        <View>
-          <Text style={{ textAlign: 'center', paddingTop: 10, }}>No matching items found.</Text>
-        </View>
-      )}
 
       <FlatList
         contentContainerStyle={styles.content}
@@ -140,15 +109,6 @@ export default function Index() {
           />
         )}
       />
-
-      {showFilter && (
-        <View style={{ position: 'absolute', top: 0, left: 0, right: 0, bottom: 0,}}>
-          <FilterComponent
-            onClose={() => setShowFilter(false)}
-            onSelectSort={handleSort}
-          />
-        </View>
-      )}
     </SafeAreaView>
   );
 }
--- app/login.tsx
+++ app/login.tsx
@@ -11,7 +11,12 @@ export default function SignIn() {
   const handleSignIn = () => {
     /* Placeholder functionality */
     console.log("Email:", form.email);
-    console.log("Passowrd:", form.password)
+    console.log("Password:", form.password)
+  };
+
+  const handleResetPassword = () => {
+    /* Placeholder functionality */
+    console.log("Password reset received")
   };
 
   const handleSignUpRedirect = () => {
@@ -20,52 +25,62 @@ export default function SignIn() {
   };
 
   return (
-    <SafeAreaView style={{ flex: 1, padding: 24 }}>
-      <View style={styles.header}>
-        <Image
-          source={{uri: 'https://play-lh.googleusercontent.com/tdLFU-DYgwlSCve40R96Ky9O2CnHsQzrV-EcDwd_omjBTkWot6hDYk4E5Vi3jeTCWw=w480-h960' }}
-          alt="Craigslist Logo"
-          style={styles.headerImg}
-        />
-        <Text style={styles.title}>Sign in to Craigslist</Text>
-        <Text style={styles.subtitle}>The original online classified.</Text>
-      </View>
-
-      <View style={styles.form}>
-        <View style={styles.inputGroup}>
-          <Text style={styles.inputLabel}>Email address</Text>
-          <TextInput
-            autoCapitalize="none"
-            autoCorrect={false}
-            keyboardType="email-address"
-            style={styles.inputControl}
-            placeholder="email@example.com"
-            value={form.email}
-            onChangeText={handleEmailChange}
+    <SafeAreaView>
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <Image
+            source={{uri: 'https://play-lh.googleusercontent.com/tdLFU-DYgwlSCve40R96Ky9O2CnHsQzrV-EcDwd_omjBTkWot6hDYk4E5Vi3jeTCWw=w480-h960' }}
+            alt="Craigslist Logo"
+            style={styles.headerImg}
           />
+          <Text style={styles.title}>Sign in to Craigslist</Text>
+          <Text style={styles.subtitle}>The original online classified.</Text>
         </View>
 
-        <View style={styles.inputGroup}>
-          <Text style={styles.inputLabel}>Password</Text>
-          <TextInput
-            autoCapitalize="none"
-            secureTextEntry
-            style={styles.inputControl}
-            placeholder="*********"
-            value={form.password}
-            onChangeText={handlePasswordChange}
-          />
-        </View>
-
-        <TouchableOpacity style={styles.formAction} onPress={handleSignIn}>
-          <View style={styles.btn}>
-            <Text style={styles.btnTxt}>Sign in</Text>
+        <View style={styles.form}>
+          <View style={styles.inputGroup}>
+            <Text style={styles.inputLabel}>Email address</Text>
+            <TextInput
+              accessibilityLabel="Email address"
+              accessibilityRole="text"
+              autoCapitalize="none"
+              autoCorrect={false}
+              keyboardType="email-address"
+              style={styles.inputControl}
+              placeholder="email@example.com"
+              value={form.email}
+              onChangeText={handleEmailChange}
+            />
           </View>
-        </TouchableOpacity>
 
-        <TouchableOpacity style={styles.footer} onPress={handleSignUpRedirect}>
-            <Text> Don't have an account? Sign up</Text>
-        </TouchableOpacity>
+          <View style={styles.inputGroup}>
+            <Text style={styles.inputLabel}>Password</Text>
+            <TextInput
+              accessibilityLabel="Password"
+              accessibilityRole="text"
+              autoCapitalize="none"
+              secureTextEntry
+              style={styles.inputControl}
+              placeholder="*********"
+              value={form.password}
+              onChangeText={handlePasswordChange}
+            />
+          </View>
+
+          <TouchableOpacity style={styles.formAction} onPress={handleSignIn}>
+            <View style={styles.btn}>
+              <Text style={styles.btnTxt}>Sign in</Text>
+            </View>
+          </TouchableOpacity>
+
+          <TouchableOpacity onPress={handleResetPassword}>
+            <Text style={styles.formLink}>Forgot password?</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.footer} onPress={handleSignUpRedirect}>
+              <Text> Don't have an account? Sign up</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     </SafeAreaView>
   )
@@ -75,7 +90,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 24,
-    backgroundColor: 'green'
   },
   header: {
     marginVertical: 24,
@@ -116,7 +130,13 @@ const styles = StyleSheet.create({
 
   },
   formAction: {
-    marginVertical: 24,
+    marginTop: 4,
+    marginBottom: 12,
+  },
+  formLink: {
+    fontSize: 16,
+    fontWeight: '600',
+    textAlign: 'center'
   },
   btn: {
     backgroundColor: 'purple',
@@ -133,7 +153,8 @@ const styles = StyleSheet.create({
     color: '#fff'
   },
   footer: {
-    marginTop: 'auto',
+    marginTop: 50,
     alignItems: 'center',
+    paddingVertical: 24,
   },
 })
