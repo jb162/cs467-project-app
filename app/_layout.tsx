e1b9b976cd31b4621147a35aa4248268d5456923
Justin Baytosh <justin.baytosh@gmail.com>
2025-05-22 15:06:11 -0700

    added API calls, started implementation for copy and favorite buttons on product page

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,16 @@
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
+import Toast from 'react-native-toast-message';
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen name="+not-found" />
-      <StatusBar style="light" />
-    </Stack>
+    <>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="+not-found" />
+        <StatusBar style="light" />
+      </Stack>
+      <Toast />
+    </>
   );
 }
